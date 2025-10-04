114da61377b7b7ca8424d686fed4b4276d96c381
Hung Nguyen Quoc <nguyenquochung291288@gmail.com>
2025-10-04 15:12:05 +0700

    import fixture

--- tests/tests/login.spec.ts
+++ tests/tests/login.spec.ts
@@ -1,18 +1,8 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../pages/login.page';
+import { test } from '../../fixtures/fixtures';
 import { getUrl } from '../../utils/urls';
-import { DashboardPage } from '../../pages/dashboard.page';
-
-let loginPage: LoginPage;
-let dashboardPage: DashboardPage;
-
-test.beforeEach(async ({ page }) => {
-  loginPage = new LoginPage(page);
-  dashboardPage = new DashboardPage(page);
-});
 
 test.describe('Login tests', () => {
-  test("Login with admin account successfully", async ({ page }) => {
+  test("Login with admin account successfully", async ({ loginPage, dashboardPage  }) => {
     await test.step('Navigate to login page', async () => {
       await loginPage.goto(getUrl("orangeHRM"));  
   });
